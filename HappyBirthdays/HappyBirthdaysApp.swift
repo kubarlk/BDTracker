dc923d27a046bcade0adcb5501754e2bde0dcd28
Kirill Kubarskiy <45077920+kubarlk@users.noreply.github.com>
2023-05-02 20:41:35 +0300

    Initial Commit

---
+++ HappyBirthdays/HappyBirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBirthdaysApp.swift
+//  HappyBirthdays
+//
+//  Created by Kirill Kubarskiy on 2.05.23.
+//
+
+import SwiftUI
+
+@main
+struct HappyBirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
